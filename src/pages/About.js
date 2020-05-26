46477aeb053e135dcd15aa629b6dc758cc7cca1b
Kuharenko <iamksergey@mail.ru>
2020-05-26 10:24:08 +0300

    master

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+    <div className="jumbotron">
+        <h1 className="display-4">Лучшее React приложение</h1>
+        <p className="lead">
+            Версия приложения <strong>1.0.42</strong>
+        </p>
+    </div>
+);
+
+export default About;
